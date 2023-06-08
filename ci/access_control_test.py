b74b9601c150d24a6fe134cc0af900b6712ef5c4
fengliangjun <f00812941@china.huawei.com>
2023-06-08 20:11:18 +0800

    fix ut

--- ci/access_control_test.py
+++ ci/access_control_test.py
@@ -1,15 +1,22 @@
 from pathlib import Path
 import os
+import sys
 
-BASE_DIR = Path(__file__).absolute().parent.parent
-TEST_DIR = os.path.join(BASE_DIR, 'tests')
+if __name__ == "__main__":
+    BASE_DIR = Path(__file__).absolute().parent.parent
+    TEST_DIR = os.path.join(BASE_DIR, 'tests')
 
 
-# gpt test
-shell_file = os.path.join(TEST_DIR, "st", "test_gpt", "test_gpt_ptd.sh")
-os.system("sh {}".format(shell_file))
+    # gpt test
+    shell_file = os.path.join(TEST_DIR, "st", "test_gpt", "test_gpt_ptd.sh")
+    os.system("sh {}".format(shell_file))
 
-# llama test
-shell_file = os.path.join(TEST_DIR, "st", "test_llama", "test_llama_ptd.sh")
-os.system("sh {}".format(shell_file))
+    # llama test
+    shell_file = os.path.join(TEST_DIR, "st", "test_llama", "test_llama_ptd.sh")
+    os.system("sh {}".format(shell_file))
+
+    if os.path.exists("./log.txt"):
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
