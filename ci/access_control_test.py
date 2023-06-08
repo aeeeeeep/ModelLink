a0c393a63a97a84bd9b0f5e904fa02f7e4a009d4
fengliangjun <f00812941@china.huawei.com>
2023-06-08 19:02:03 +0800

    fix test

---
+++ ci/access_control_test.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).absolute().parent.parent
+TEST_DIR = os.path.join(BASE_DIR, 'tests')
+
+
+# gpt test
+shell_file = os.path.join(TEST_DIR, "st", "test_gpt", "test_gpt_ptd.sh")
+os.system("sh {}".format(shell_file))
+
+# llama test
+shell_file = os.path.join(TEST_DIR, "st", "test_llama", "test_llama_ptd.sh")
+os.system("sh {}".format(shell_file))
+
+
--- tests/st/test_llama/test_llama_ptd.sh
+++ tests/st/test_llama/test_llama_ptd.sh
@@ -2,4 +2,4 @@ export LD_LIBRARY_PATH=/usr/local/lib:/root/miniconda3/lib:$LD_LIBRARY_PATH
 export HCCL_CONNECT_TIMEOUT=1200
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
-pytest ./tests/st/test_llama/test_llama_ptd.py
+python ./tests/st/test_llama/test_llama_ptd.py
