7e9e698f250cce342e125e146008458a17a955fb
Zhihong Chen <892708442@qq.com>
2023-10-08 11:43:38 +0800

    删除错误图片文件夹
    Signed-off-by: Zhihong Chen <892708442@qq.com>

--- ascendspeed/core/model_parallel_config.py
+++ ascendspeed/core/model_parallel_config.py
@@ -146,8 +146,8 @@ class ModelParallelConfig:
     param_sync_func: Callable = None
 
     def __post_init__(self):
-        """
-        Python dataclass method that is used to modify attributes after initialization.
+        """ Python dataclass method that is used to modify attributes after initialization.
+            See https://docs.python.org/3/library/dataclasses.html#post-init-processing for more details.
         """
         if self.sequence_parallel:
             if self.tensor_model_parallel_size <= 1:
