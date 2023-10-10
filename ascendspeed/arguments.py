181a6004a41c8dc628f1ac63916af96d0d1cf81b
chantcalf <magicallc@126.com>
2023-10-10 15:13:51 +0800

    add GQA arguments

--- ascendspeed/arguments.py
+++ ascendspeed/arguments.py
@@ -396,6 +396,9 @@ def _add_network_size_args(parser):
                        'attention. This is set to '
                        '   args.hidden_size // args.num_attention_heads '
                        'if not provided.')
+    group.add_argument('--group-query-attention', action='store_true',
+                       help='Use group-query attention.')
+    group.add_argument('--num-query-groups', type=int, default=1)
     group.add_argument('--embed-layernorm', action='store_true',
                        help='Use layernorm for embedding.')
     group.add_argument('--max-position-embeddings', type=int, default=None,
