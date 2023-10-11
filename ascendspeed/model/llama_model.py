2b970bfd3f3e6d6552d4c01d8eac2c802df5acf1
chantcalf <magicallc@126.com>
2023-10-11 08:52:13 +0800

    llama_model 优化rmsnorm，bf16不用转为fp32计算

--- ascendspeed/model/llama_model.py
+++ ascendspeed/model/llama_model.py
@@ -104,14 +104,14 @@ class RMSNorm(torch.nn.Module):  # for cpu
         setattr(self.weight, 'sequence_parallel', sequence_parallel)
 
     def forward(self, hidden_states):
-        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-
-        # convert into half-precision if necessary
-        if self.weight.dtype in [torch.float16, torch.bfloat16]:
-            hidden_states = hidden_states.to(self.weight.dtype)
+        if self.weight.dtype == torch.float16:
+            hidden_states = hidden_states.float()
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        variance = torch.rsqrt(variance + self.variance_epsilon)
+        if self.weight.dtype == torch.float16:
+            variance = variance.half()
+        hidden_states = hidden_states * variance
         hidden_states = self.weight * hidden_states
-
         return hidden_states
 
 
