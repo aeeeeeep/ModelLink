5f12de69fdce4f3d1c11762aa9980769d47773af
yangyuan667 <yangyuan42@huawei.com>
2023-11-23 10:04:55 +0800

    varpack

--- ascendspeed/te/ops/csrc/adapter.cpp
+++ ascendspeed/te/ops/csrc/adapter.cpp
@@ -148,18 +148,17 @@ void TECommand::Output(std::vector<at::Tensor> &output)
     context->SetExecuteStream(stream);
     auto ins = variantPack.inTensors;
     auto outs = variantPack.outTensors;
-    auto op = this->operation;
-    auto te_call = [&op, &ins, &outs, &workspaceTensor, workspaceSize, &context]() -> int {
+    auto te_call = [this, ins, outs, workspaceTensor, workspaceSize, context]() -> int {
         atb::VariantPack varPack;
         varPack.inTensors.resize(ins.size());
         varPack.outTensors.resize(outs.size());
         for (size_t i = 0; i < ins.size(); ++i) {
-            varPack.inTensors.at(i) = ins.at(i);
+            varPack.inTensors.push_back(ins.at(i));
         }
         for (size_t i = 0; i < outs.size(); ++i) {
-            varPack.outTensors.at(i) = outs.at(i);
+            varPack.outTensors.push_back(outs.at(i));
         }
-        auto api_ret = op->Execute(varPack, (uint8_t *)workspaceTensor.storage().data(), workspaceSize, context);
+        auto api_ret = this->operation->Execute(varPack, (uint8_t *)workspaceTensor.storage().data(), workspaceSize, context);
         TORCH_CHECK(api_ret == 0, "execute failed");
         // atb::DestroyContext(context);
         return api_ret;
