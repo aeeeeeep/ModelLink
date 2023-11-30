1ca154767c1f737c2fb6296536dcb3baedcdec37
yangyuan667 <yangyuan42@huawei.com>
2023-11-30 19:25:51 +0800

    add fix

--- ascendspeed/te/ops/csrc/flash_attention.cpp
+++ ascendspeed/te/ops/csrc/flash_attention.cpp
@@ -136,7 +136,7 @@ std::tuple<at::Tensor, at::Tensor> fa(const at::Tensor &query, const at::Tensor 
 
     auto acl_call = [op, contextPtr, variantPack, workspacePtr, workspaceSize]() -> int {
         auto st = op->Execute(variantPack, (uint8_t *)workspacePtr, workspaceSize, contextPtr);
-        DestroyOperation(op);
+        // DestroyOperation(op);
         return 0;
     };
 
@@ -249,7 +249,7 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> fag(const at::Tensor &dy, const a
 
     auto acl_call = [op, contextPtr, variantPack, workspacePtr, workspaceSize]() -> int {
         auto st = op->Execute(variantPack, (uint8_t *)workspacePtr, workspaceSize, contextPtr);
-        DestroyOperation(op);
+        // DestroyOperation(op);
         return 0;
     };
     at_npu::native::OpCommand cmd;
