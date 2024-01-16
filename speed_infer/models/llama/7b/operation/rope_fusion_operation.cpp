edfb8750be781e6dda76a5233ac85adffaefcc27
yangyifei <yangyifei15@huawei.com>
2024-01-16 14:45:25 +0800

    adapt llama rope

--- speed_infer/models/llama/7b/operation/rope_fusion_operation.cpp
+++ speed_infer/models/llama/7b/operation/rope_fusion_operation.cpp
@@ -26,26 +26,11 @@ enum RopeFusionTensorId {
     IN_SEQLEN_ID,        // [batch], int32
     OUT_QEMBEDDED_ID,    // [batch, seqlen, headNum, headDim], half
     OUT_KEMBEDDED_ID,    // [batch, seqlen, headNum, headDim], half
-    INTERMEDIATE_COS_ID, // [batch, seqLen, headDim], half
-    INTERMEDIATE_SIN_ID, // [batch, seqLen, headDim], half
 };
 
 static const uint64_t IN_TENSOR_COUNT = 6;
 static const uint64_t OUT_TENSOR_COUNT = 2;
-static const uint64_t INTERMEDIATE_TENSOR_COUNT = 2;
-static const uint64_t NODE_COUNT = 3;
-
-void Squeeze(const atb::Dims &oldShape, atb::Dims &newShape)
-{
-    if (oldShape.dims[0] == 1) {
-        newShape.dimNum = oldShape.dimNum - 2; // dim:4 -> 2
-        for (size_t i = 0; i < newShape.dimNum; i++) {
-            newShape.dims[i] = oldShape.dims[i + 2];
-        }
-    } else {
-        newShape = oldShape;
-    }
-}
+static const uint64_t NODE_COUNT = 1;
 
 void RopeReshapeFunc(const atb::Dims &oldShape, atb::Dims &newShape)
 {
@@ -60,32 +45,15 @@ atb::Status RopeFusionOperation(const RopeFusionParam &param, atb::Operation **o
     atb::GraphParam opGraph;
     opGraph.inTensorNum = IN_TENSOR_COUNT;
     opGraph.outTensorNum = OUT_TENSOR_COUNT;
-    opGraph.internalTensorNum = INTERMEDIATE_TENSOR_COUNT;
     opGraph.nodes.resize(NODE_COUNT);
 
     size_t nodeId = 0;
-    auto &embeddingCosNode = opGraph.nodes.at(nodeId++);
-    auto &embeddingSinNode = opGraph.nodes.at(nodeId++);
     auto &ropeNode = opGraph.nodes.at(nodeId++);
-
-    atb::infer::GatherParam embeddingCosParam;
-    CreateOperation(embeddingCosParam, &embeddingCosNode.operation);
-    embeddingCosNode.inTensorIds = {IN_COS_TABLE_ID, IN_POSITION_IDS_ID};
-    embeddingCosNode.outTensorIds = {INTERMEDIATE_COS_ID};
-    embeddingCosNode.inTensorReshapeFuncs.resize(embeddingCosNode.inTensorIds.size());
-    embeddingCosNode.inTensorReshapeFuncs.at(0) = &Squeeze; // [maxseqLen, headDim]
-
-    atb::infer::GatherParam embeddingSinParam;
-    CreateOperation(embeddingSinParam, &embeddingSinNode.operation);
-    embeddingSinNode.inTensorIds = {IN_SIN_TABLE_ID, IN_POSITION_IDS_ID};
-    embeddingSinNode.outTensorIds = {INTERMEDIATE_SIN_ID};
-    embeddingSinNode.inTensorReshapeFuncs.resize(embeddingSinNode.inTensorIds.size());
-    embeddingSinNode.inTensorReshapeFuncs.at(0) = &Squeeze;
 
     atb::infer::RopeParam ropeParam;
     ropeParam.rotaryCoeff = 2; // 设置旋转系数
     CreateOperation(ropeParam, &ropeNode.operation);
-    ropeNode.inTensorIds = {IN_QLAYER_ID, IN_KLAYER_ID, INTERMEDIATE_COS_ID, INTERMEDIATE_SIN_ID, IN_SEQLEN_ID};
+    ropeNode.inTensorIds = {IN_QLAYER_ID, IN_KLAYER_ID, IN_COS_TABLE_ID, IN_SIN_TABLE_ID, IN_SEQLEN_ID};
     ropeNode.outTensorIds = {OUT_QEMBEDDED_ID, OUT_KEMBEDDED_ID};
     ropeNode.inTensorReshapeFuncs = {&RopeReshapeFunc, &RopeReshapeFunc, &RopeReshapeFunc, &RopeReshapeFunc};
 
