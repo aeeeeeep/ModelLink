5d4945f6d2883c05fc8e180ebea03cc2689edfb1
niushiya <niushiya1@huawei.com>
2024-01-18 08:14:30 +0000

    !462 LLaMA page_attention_model 浮点&量化 绿区迁移蓝区 Merge pull request !462 from niushiya/master

--- speed_infer/models/llama_pa/layer/anti_quant_paged_attention_layer.h
+++ speed_infer/models/llama_pa/layer/anti_quant_paged_attention_layer.h
@@ -13,19 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef ATB_SPEED_MODELS_LLAMA_PA_PA_LAYER_H
-#define ATB_SPEED_MODELS_LLAMA_PA_PA_LAYER_H
+#ifndef ATB_SPEED_MODELS_LLAMA_QUANT_PA_LAYER_H
+#define ATB_SPEED_MODELS_LLAMA_QUANT_PA_LAYER_H
 
 #include <atb/atb_infer.h>
 #include <nlohmann/json.hpp>
-
-#include "atb_speed/base/hosttensor_binder.h"
 #include "atb_speed/log.h"
+#include "atb_speed/base/hosttensor_binder.h"
 #include "atb_speed/utils/str_split.h"
 
 namespace atb_speed {
 namespace llama_pa {
-struct PALayerParam {
+struct QuantPALayerParam {
     int rank = 0;
     int rankSize = 1;
     float rmsNormEps = 0;
@@ -34,45 +33,21 @@ struct PALayerParam {
     bool transposedWeight = false;
     bool isPrefill = false;
     std::string backend = "hccl";
-    std::string model = "llama_65b";
+    std::string model = "llama";
+    bool isBF16 = false;
+
+    // 量化参数
+    float qkvInputScale = 1;
+    int qkvInputOffset = 0;
+    float denseInputScale = 1;
+    int denseInputOffset = 0;
+    float selfLnInputScale = 1;
+    int selfLnInputOffset = 0;
+    float ffnOutInputScale = 1;
+    int ffnOutInputOffset = 0;
 };
 
-enum LayerPATensorId : int {
-    IN_HIDDENSTATES = 0,
-    IN_NORMWEIGHT, // weights
-    IN_QKVMIXEDLINEARWEIGHT,
-    IN_SELFOUTLINEARWEIGHT,
-    IN_SELFOUTNORMWEIGHT,
-    IN_MLPGATEUPWEIGHT,
-    IN_MLPDOWNWEIGHT,
-    IN_POSITIONIDS, // inputs
-    IN_COSEMBED,
-    IN_SINEMBED,
-    IN_ATTENTIONMASK,
-    IN_K_CACHE, // kvcache
-    IN_V_CACHE,
-    IN_BLOCK_TABLES,
-    IN_SLOTS,
-    IN_INPUT_LENGTHS,
-
-    OUT_LAYEROUT,
-
-    INTERMIDATE_INPUTNORMOUT,
-    INTERMIDATE_QKVMIXEDLINEAROUT,
-    INTERMIDATE_MIXEDQ,
-    INTERMIDATE_MIXEDK,
-    INTERMIDATE_MIXEDV,
-    INTERMIDATE_POSITIONEMBEDQ,
-    INTERMIDATE_POSITIONEMBEDK,
-
-    INTERMIDATE_ATTENTIONOUT, // attention output
-    INTERMIDATE_SELFLINEAROUT,
-    INTERMIDATE_SELFRESIDUALADDOUT,
-    INTERMIDATE_SELFNORMOUT,
-    INTERMIDATE_MLPOUT,
-};
-
-static void from_json(const nlohmann::json &paramJson, PALayerParam &param)
+static void from_json(const nlohmann::json &paramJson, QuantPALayerParam &param)
 {
     paramJson.at("rmsNormEps").get_to(param.rmsNormEps);
     paramJson.at("headNum").get_to(param.headNum);
@@ -92,23 +67,32 @@ static void from_json(const nlohmann::json &paramJson, PALayerParam &param)
     if (paramJson.contains("backend")) {
         paramJson.at("backend").get_to(param.backend);
     }
+    // 量化参数
+    param.qkvInputScale = paramJson["qkvInputScale"].get<float>();
+    param.qkvInputOffset = paramJson["qkvInputOffset"].get<int>();
+    param.denseInputScale = paramJson["denseInputScale"].get<float>();
+    param.denseInputOffset = paramJson["denseInputOffset"].get<int>();
+    param.selfLnInputScale = paramJson["selfLnInputScale"].get<float>();
+    param.selfLnInputOffset = paramJson["selfLnInputOffset"].get<int>();
+    param.ffnOutInputScale = paramJson["ffnOutInputScale"].get<float>();
+    param.ffnOutInputOffset = paramJson["ffnOutInputOffset"].get<int>();
 }
 
-atb::Status PALayer(const PALayerParam &param, atb::Operation **operation);
+atb::Status QuantPALayer(const QuantPALayerParam &param, atb::Operation **operation);
 
-static atb::Operation *CreatePALayer(const nlohmann::json &paramJson)
+static atb::Operation *CreateQuantPALayer(const nlohmann::json &paramJson)
 {
     ATB_LOG(INFO) << GetFuncNameAndNameSpace(__PRETTY_FUNCTION__);
     atb::Operation *op;
-    PALayer(paramJson.get<PALayerParam>(), &op);
+    QuantPALayer(paramJson.get<QuantPALayerParam>(), &op);
     return op;
 }
 
-class FlashAttentionHostBinder : public HostTensorBinder {
+class QuantFlashAttentionHostBinder : public HostTensorBinder {
 public:
-    FlashAttentionHostBinder();
+    QuantFlashAttentionHostBinder();
 
-    virtual ~FlashAttentionHostBinder();
+    virtual ~QuantFlashAttentionHostBinder();
 
     void ParseParam(const nlohmann::json &paramJson) override;
 
