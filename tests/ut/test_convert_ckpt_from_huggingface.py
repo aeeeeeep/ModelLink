c059ef0c53c70f1bc998d7affb1e85069d1f0b2e
g00841271 <guoxinjie1@huawei.com>
2024-01-30 15:07:11 +0800

    add ckpt convert from HF ut

--- tests/ut/test_convert_ckpt_from_huggingface.py
+++ tests/ut/test_convert_ckpt_from_huggingface.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import os
 import subprocess
+from pathlib import Path
 import torch
 
 from ascendspeed import megatron_adaptor
@@ -36,17 +37,20 @@ class TestConvertCkptFromHuggingface(unittest.TestCase):
         embedding, final_norm, output and encoder. In the encoder, there will be some different layers to
         compose the unique transformer layer and all these layer stack to compose the entity of the model.
         """
-        file_path = os.path.join("", "tools/checkpoint/utils.py")
+        base_dir = Path(__file__).absolute().parent.parent.parent
+        file_path = os.path.join(base_dir, "tools/checkpoint/utils.py")
         arguments = sys.argv[1:]
         subprocess.run(["python", file_path] + arguments)
         output_dir = os.path.join(sys.argv[12], "iter_0000001")
         weight_content = torch.load(os.path.join(output_dir, "mp_rank_00/model_optim_rng.pt"))
-        weight_common_content = weight_content['model']['language_model']
+        weight_common_content = weight_content['model']['language_model'] # extract commmon content
 
+        # embedding, encoder, output_layer is three out layers.
         self.assertEqual(len(os.listdir(output_dir)), int(sys.argv[8]))
         self.assertEqual(weight_common_content['embedding']['word_embedding']['weight'].size(), torch.Size([4000, 4096]))
         self.assertEqual(weight_common_content['encoder'], ['final_norm.weight'].size(), torch.Size([4096]))
 
+        # encoder has a common final_norm and each one has folliowing six layers
         weight_common_content['encoder'].pop('final_norm.weight')
         self.assertEqual(len(weight_common_content['encoder']) / 6, 32)
         self.assertEqual(weight_common_content['encoder']['layer.0.self_attention.query_key_value.weight'].size(), torch.Size([1536, 4096]))
