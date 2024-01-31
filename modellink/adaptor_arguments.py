42553b60b398f4b1c5470aadcb980319939a03b5
huangyiming <1310439159@qq.com>
2024-01-31 09:55:25 +0800

    add bloom 7b pretrain, generation and evaluation

--- modellink/adaptor_arguments.py
+++ modellink/adaptor_arguments.py
@@ -1,4 +1,6 @@
 from functools import wraps
+import argparse
+
 import megatron
 
 
@@ -31,6 +33,8 @@ def process_args(parser):
     parser.conflict_handler = 'resolve'
     parser = _add_lora_args(parser)
     parser = _add_data_args(parser)
+    parser = _add_alibi_args(parser)
+    parser = _add_network_size_args(parser)
     return parser
 
 
@@ -60,5 +64,39 @@ def _add_data_args(parser):
     group = parser.add_argument_group(title='data')
     group.add_argument('--is-instruction-dataset', action='store_true', help='use instruction dataset or not')
     group.add_argument('--variable-seq-lengths', action='store_true', help='Use variable seq lengths or not.')
-    
+
+def add_parser_argument_choices_value(parser, argument_name, value):
+    for i, action in enumerate(parser._actions):
+        if isinstance(action, argparse._ArgumentGroup):
+            add_parser_argument_choices_value(action, argument_name)
+        elif isinstance(action, argparse.Action) and argument_name in action.option_strings:
+            action.choices.append(value)
+
+
+def _add_network_size_args(parser):
+    group = parser.add_argument_group(title='network_size_args')
+    group.add_argument('padded_vocab_size',
+                       action='store_true',
+                       default=None,
+                       help='set padded vocab size')
+    group.add_argument('embed_layernorm',
+                       action='store_true',
+                       default=True,
+                       help='set padded vocab size'
+                       )
+    return parser
+
+
+def _add_alibi_args(parser):
+    add_parser_argument_choices_value(parser, "--position-embedding-type", 'alibi')
+
+    group = parser.add_argument_group(title='alibi')
+    group.add_argument('--square-alibi-mask',
+                       action='store_true',
+                       default=False,
+                       help='attention mask of alibi is squared')
+    group.add_argument('--fill-neg-inf',
+                       action='store_true',
+                       default=False,
+                       help='fill alibi with negative inf')
     return parser
