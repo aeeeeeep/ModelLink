eac06c30250177b73ffe47f89a6e1a1b363b3d5f
xuyexiong <xuyexiong@huawei.com>
2024-02-01 14:59:19 +0800

    [feature] telechat 浮点、量化双芯适配

---
+++ mindie_ref/mindie_llm/atb_models/models/telechat/layer/float_layer.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ATB_SPEED_MODELS_TELECHAT_FLOAT_LAYER_H
+#define ATB_SPEED_MODELS_TELECHAT_FLOAT_LAYER_H
+
+#include "atb/atb_infer.h"
+#include "atb_speed/log.h"
+#include "nlohmann/json.hpp"
+
+namespace atb_speed {
+namespace telechat {
+struct FloatFALayerParam {
+    double rmsNormEps = 0;
+    int headNum = 0;
+    int dk = 0;
+    int rank = 0;
+    int rankSize = 1;
+};
+
+atb::Status FloatFALayer(const FloatFALayerParam &param, atb::Operation **operation);
+
+}  // namespace telechat
+}  // namespace atb_speed
+
+#endif
