1a867556545d6b3d033b5948e123b6ab53a9c3c7
michaellee123 <lifangyuan13@huawei.com>
2024-02-06 17:47:08 +0800

    copyright修改

--- mindie_ref/mindie_llm/atb_models/pytorch/examples/gptneox20b/modeling_gpt_neox_ascend.py
+++ mindie_ref/mindie_llm/atb_models/pytorch/examples/gptneox20b/modeling_gpt_neox_ascend.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-#  Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+# Copyright 2022 EleutherAI The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
