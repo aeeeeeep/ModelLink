4a0b712546588be06a9d138be17bf4bdee1074cc
Dingli <dingli40@huawei.com>
2024-02-20 10:27:25 +0800

    fix codegeex2

--- mindie_ref/mindie_llm/atb_models/pytorch/examples/codegeex2/6b/humanevalx.py
+++ mindie_ref/mindie_llm/atb_models/pytorch/examples/codegeex2/6b/humanevalx.py
@@ -12,9 +12,9 @@ from tqdm import tqdm
 from transformers.utils import check_min_version
 from torch_npu.contrib import transfer_to_npu
 
-sys.path.insert(0, Path(__file__, '../../../chatglm2/6b').resolve())
+sys.path.insert(0, str(Path(__file__, '../../../chatglm2/6b').resolve()))
 from main import get_model
-sys.path.insert(0, Path(__file__,  '../CodeGeeX2/evaluation').resolve())
+sys.path.insert(0, str(Path(__file__,  '../CodeGeeX2/evaluation').resolve()))
 from generation import CodeStoppingCriteria
 from utils import Logger, read_dataset, process_extra_prompt, is_code_generation_finished, cleanup_code
 
