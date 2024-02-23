05acf47b4d68407492944f0f54f33c368b69525b
wqh17101 <597935261@qq.com>
2024-02-23 16:30:35 +0800

    baichuan2 modeling优化

--- mindie_ref/mindie_llm/atb_models/pytorch/examples/baichuan2/7b/modeling_baichuan_ascend.py
+++ mindie_ref/mindie_llm/atb_models/pytorch/examples/baichuan2/7b/modeling_baichuan_ascend.py
@@ -527,7 +527,7 @@ class BaichuanModel(BaichuanPreTrainedModel):
             "layerNum": config.num_hidden_layers,
             "rank": self.rank,
             "rankSize": self.world_size,
-            "backend": os.getenv("BACKEND", "hccl")
+            "backend": "lccl" if IS_ND else "hccl"
         })
         self.max_position_embeddings = int(os.getenv("MAX_SEQ_LEN", config.max_position_embeddings))
         self.acl_fa_operation = torch.classes.ModelTorch.ModelTorch("baichuan2_7b_FlashAttentionRopeModel")
