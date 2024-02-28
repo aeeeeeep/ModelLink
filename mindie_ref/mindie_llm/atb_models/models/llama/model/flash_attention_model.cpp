48e20c57ba0f8b0fa0a0474ab6b2c56b699a08d5
yangyifei626 <yangyifei15@huawei.com>
2024-02-28 11:21:17 +0800

    fixed quant problem

--- mindie_ref/mindie_llm/atb_models/models/llama/model/flash_attention_model.cpp
+++ mindie_ref/mindie_llm/atb_models/models/llama/model/flash_attention_model.cpp
@@ -22,6 +22,7 @@
 
 namespace atb_speed {
 namespace llama {
+const int TEST=0;
 const int QUANT_WEIGHT_COUNT_PER_LAYER = 25;
 const int SPARSE_WEIGHT_COUNT_PER_LAYER = 32;
 const int FLOAT_WEIGHT_COUNT_PER_LAYER = 9;
@@ -255,10 +256,12 @@ int64_t FlashAttentionModel::BuildGraph()
             atb_speed::llama::FlashAttentionLayerParam quantModelParam;
             quantModelParam.rmsNormEps = param_.rmsNormEps;
             quantModelParam.headNum = param_.headNum;
+            quantModelParam.kvHeadNum = param_.kvHeadNum;
             quantModelParam.dk = param_.dk;
             quantModelParam.model = "llama13b";
             quantModelParam.rank = param_.rank;
             quantModelParam.rankSize = param_.rankSize;
+            quantModelParam.isTriuMask = param_.isTriuMask;
             quantModelParam.backend = param_.backend;
             quantModelParam.quantModel = true;
             quantModelParam.isEncoder = param_.isEncoder;
@@ -308,10 +311,12 @@ int64_t FlashAttentionModel::BuildGraph()
             atb_speed::llama::FlashAttentionLayerParam sparseModelParam;
             sparseModelParam.rmsNormEps = param_.rmsNormEps;
             sparseModelParam.headNum = param_.headNum;
+            sparseModelParam.kvHeadNum = param_.kvHeadNum;
             sparseModelParam.dk = param_.dk;
             sparseModelParam.model = "llama13b";
             sparseModelParam.rank = param_.rank;
             sparseModelParam.rankSize = param_.rankSize;
+            sparseModelParam.isTriuMask = param_.isTriuMask;
             sparseModelParam.backend = param_.backend;
             sparseModelParam.sparseModel = true;
             sparseModelParam.isEncoder = param_.isEncoder;
