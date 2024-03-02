bdb87f90158ce997c7e65a653b355a16b8f3d8cc
chenxinyi (F) <katrina.cxy@gmail.com>
2024-03-02 16:36:05 +0800

    添加LLaMa C++组图，适配新的Common Op

--- mindie_ref/mindie_llm/atb_models/models/llama_parallel/layer/decoder_layer.h
+++ mindie_ref/mindie_llm/atb_models/models/llama_parallel/layer/decoder_layer.h
@@ -16,6 +16,7 @@
 #ifndef ATB_SPEED_MODELS_LLAMA_PARALLEL_DECODER_LAYER_H
 #define ATB_SPEED_MODELS_LLAMA_PARALLEL_DECODER_LAYER_H
 
+#include <vector>
 #include "nlohmann/json.hpp"
 
 #include "atb/atb_infer.h"
@@ -38,45 +39,58 @@ struct DecoderLayerParam {
     int rank = 0;
     int worldSize = 1;
     std::string backend = "hccl";
-    std::string rankTableFile = "";
     std::vector<int> seqLen;
     std::vector<int> tokenOffset;
+    std::vector<int> packQuantType = {};  // 两个元素，第一个元素代表QKV pack的量化类型，第二个元素代表MLP pack的量化类型
+    // 七个元素，分别代表q，k，v，self attention out，gate，up，down linear的类型
+    std::vector<int> linearQuantType = {};
 };
 
 enum DecoderLayerTensorIdx : uint32_t {
     IN_HIDDEN_STATES = 0,               // shape: FA: [batchSize, seqLen, maxPositionEmbeddings] PA: [seqLen, hiddenSize]
     IN_INPUT_NORM_WEIGHT,               // shape: [hiddenSize]
+    IN_INPUT_NORM_QUANT_WEIGHT,
+    IN_INPUT_NORM_QUANT_BIAS,
     IN_QKV_WEIGHT_0,                    // Pack: shape: MHA [3 * numAttentionHeadsPerRank * hiddenSizePerAttentionHead, hiddenSize] GQA [(numAttentionHeadsPerRank + 2 * numKeyValueHeadsPerRank) * hiddenSizePerAttentionHead, hiddenSize]
                                         // No pack: (Q) shape: [numAttentionHeadsPerRank * hiddenSizePerAttentionHead, hiddenSize]
-    IN_QKV_SCALE_0,                     // Quant所需权重
-    IN_QKV_OFFSET_0,                    // Quant所需权重
+    IN_QKV_DEOFFSET_0,                  // Quant所需权重
     IN_QKV_DESCALE_0,                   // Quant所需权重
+    IN_QKV_OFFSET_0,                    // Quant所需权重
+    IN_QKV_SCALE_0,                     // Quant所需权重
     IN_QKV_WEIGHT_1,                    // Pack: no usage; No pack: (K) shape: [numKeyValueHeadsPerRank * hiddenSizePerAttentionHead, hiddenSize]
-    IN_QKV_SCALE_1,                     // Quant所需权重
-    IN_QKV_OFFSET_1,                    // Quant所需权重
+    IN_QKV_DEOFFSET_1,                  // Quant所需权重
     IN_QKV_DESCALE_1,                   // Quant所需权重
+    IN_QKV_OFFSET_1,                    // Quant所需权重
+    IN_QKV_SCALE_1,                     // Quant所需权重
     IN_QKV_WEIGHT_2,                    // Pack: no usage; No pack: (V) shape: [numKeyValueHeadsPerRank * hiddenSizePerAttentionHead, hiddenSize]
-    IN_QKV_SCALE_2,                     // Quant所需权重
-    IN_QKV_OFFSET_2,                    // Quant所需权重
+    IN_QKV_DEOFFSET_2,                  // Quant所需权重
     IN_QKV_DESCALE_2,                   // Quant所需权重
+    IN_QKV_OFFSET_2,                    // Quant所需权重
+    IN_QKV_SCALE_2,                     // Quant所需权重
     IN_ATTENTION_OUT_WEIGHT,            // shape: [hiddenSize, numAttentionHeadsPerRank * hiddenSizePerAttentionHead]
-    IN_ATTENTION_OUT_SCALE,             // Quant所需权重
-    IN_ATTENTION_OUT_OFFSET,            // Quant所需权重
+    IN_ATTENTION_OUT_DEOFFSET,          // Quant所需权重
     IN_ATTENTION_OUT_DESCALE,           // Quant所需权重
+    IN_ATTENTION_OUT_OFFSET,            // Quant所需权重
+    IN_ATTENTION_OUT_SCALE,             // Quant所需权重
     IN_ATTENTION_NORM_WEIGHT,           // shape: [hiddenSize]
+    IN_ATTENTION_NORM_QUANT_WEIGHT,
+    IN_ATTENTION_NORM_QUANT_BIAS,
     IN_MLP_WEIGHT_0,                    // Pack: shape: [2 * intermediateSizePerRank, hiddenSize]
                                         // No pack: (Gate) shape: [intermediateSizePerRank, hiddenSize]
-    IN_MLP_SCALE_0,                     // Quant所需权重
-    IN_MLP_OFFSET_0,                    // Quant所需权重
+    IN_MLP_DEOFFSET_0,                  // Quant所需权重
     IN_MLP_DESCALE_0,                   // Quant所需权重
+    IN_MLP_OFFSET_0,                    // Quant所需权重
+    IN_MLP_SCALE_0,                     // Quant所需权重
     IN_MLP_WEIGHT_1,                    // Pack: no usage; No pack: (Up) shape: [intermediateSizePerRank, hiddenSize]
-    IN_MLP_SCALE_1,                     // Quant所需权重
-    IN_MLP_OFFSET_1,                    // Quant所需权重
+    IN_MLP_DEOFFSET_1,                  // Quant所需权重
     IN_MLP_DESCALE_1,                   // Quant所需权重
+    IN_MLP_OFFSET_1,                    // Quant所需权重
+    IN_MLP_SCALE_1,                     // Quant所需权重
     IN_MLP_DOWN_WEIGHT,                 // shape: [hiddenSize, intermediateSizePerRank]
-    IN_MLP_DOWN_SCALE,                  // Quant所需权重
-    IN_MLP_DOWN_OFFSET,                 // Quant所需权重
+    IN_MLP_DOWN_DEOFFSET,               // Quant所需权重
     IN_MLP_DOWN_DESCALE,                // Quant所需权重
+    IN_MLP_DOWN_OFFSET,                 // Quant所需权重
+    IN_MLP_DOWN_SCALE,                  // Quant所需权重
     IN_COS_TABLE,                       // shape: FA: [batchSize * seqLen, hiddenSizePerAttentionHead] PA: [seqLen, hiddenSizePerAttentionHead]
     IN_SIN_TABLE,                       // shape: FA: [batchSize * seqLen, hiddenSizePerAttentionHead] PA: [seqLen, hiddenSizePerAttentionHead]
     IN_ATTENTION_MASK,                  // shape: FA: [batchSize, maxPositionEmbeddings, maxPositionEmbeddings] PA: [seqLen, seqLen]
@@ -84,7 +98,6 @@ enum DecoderLayerTensorIdx : uint32_t {
     IN_V_CACHE,                         // shape: FA: [batchSize, maxPositionEmbeddings, numKeyValueHeadsPerRank * hiddenSizePerAttentionHead] PA: [2622, hiddenSizePerAttentionHead, numAttentionHeadsPerRank, hiddenSizePerAttentionHead]
     IN_SEQ_LEN,                         // shape: [batchSize]
     IN_PLACE_HOLDER,                    // shape: [1]
-    IN_BETA,                            // shape: [hiddenSize]; Quant所需参数
     IN_TOKEN_OFFSET,                    // shape: [batchSize]; FA所需参数
     IN_LAYER_ID,                        // shape: [1]; FA所需参数
     IN_BLOCK_TABLES,                    // shape: [seqLen, seqLen]; PA所需参数
@@ -92,10 +105,8 @@ enum DecoderLayerTensorIdx : uint32_t {
 
     OUT_DECODER_LAYER,                  // shape: FA: [batchSize, seqLen, maxPositionEmbeddings] PA: [seqLen, hiddenSize]
 
-    INTERMEDIATE_INPUT_NORM_OUT,        // shape: PA: [seqLen, hiddenSize]
     INTERMEDIATE_ATTENTION_OUT,         // shape: PA: [seqLen, hiddenSize]
     INTERMEDIATE_RESIDUAL_ADD_OUT,      // shape: PA: [seqLen, hiddenSize]
-    INTERMEDIATE_ATTENTION_NORM_OUT,    // shape: PA: [seqLen, hiddenSize]
     INTERMEDIATE_MLP_OUT,               // shape: PA: [seqLen, hiddenSize]
 };
 
