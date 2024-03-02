cf09be71082109f343c5ae1f7fce325f97c2f962
z00581707 <zhangshufan@huawei.com>
2024-03-02 17:18:13 +0800

    aquila接服务化

--- mindie_ref/mindie_llm/atb_models/atb_llm/models/aquila/v1_7b/config.py
+++ mindie_ref/mindie_llm/atb_models/atb_llm/models/aquila/v1_7b/config.py
@@ -1,10 +1,5 @@
 # coding=utf-8
-# Copyright 2023 EleutherAI and the HuggingFace Inc. team. All rights reserved.
-#
-# This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
-# and OPT implementations in this library. It has been modified from its
-# original forms to accommodate minor architectural differences compared
-# to GPT-NeoX and OPT used by the Meta AI team that trained the model.
+# Copyright (c) 2023, HUAWEI CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +15,6 @@
 """ Aquila model configuration"""
 
 from transformers import PretrainedConfig
-
 
 
 class AquilaConfig(PretrainedConfig):
