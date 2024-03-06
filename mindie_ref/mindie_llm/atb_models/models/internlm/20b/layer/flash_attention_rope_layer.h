1c0b7f65193696b08d94ef6ce1ef11d35ec4c665
zhangjunyu2020 <zhangjunyu14@huawei.com>
2024-03-06 14:25:58 +0800

    fix：add SelfAttentionParam

--- mindie_ref/mindie_llm/atb_models/models/internlm/20b/layer/flash_attention_rope_layer.h
+++ mindie_ref/mindie_llm/atb_models/models/internlm/20b/layer/flash_attention_rope_layer.h
@@ -35,6 +35,35 @@ struct FlashAttentionRopeLayerParam {
     std::string model = "internlm_20b";
 };
 
+struct SelfAttentionParam {
+    int32_t headDim = 0;
+    int32_t headNum = 0;
+    uint32_t isTriuMask = 0;
+    float qScale = 1;  // qtensor scale before qkbmm
+    float qkScale = 1; // scale after qkbmm
+    bool batchRunStatusEnable = false;
+    int32_t kvHeadNum = 0;
+    bool isEncoder = false; // encoder for pagedAttention
+    enum CoderType : int {
+        UNDEFINED = 0,
+        ENCODER, // encoder for flashAttention
+        DECODER  // decoder for flashAttention
+    };
+    CoderType coderType = UNDEFINED;
+    bool isSupportAlibi = false;
+    bool isFp32 = false; // high precision mode
+    bool isClamp = false;
+    float clampMin = 0;
+    float clampMax = 0;
+    enum MaskType : int {
+        MASK_TYPE_UNDEFINED = 0,
+        MASK_TYPE_NORM,
+        MASK_TYPE_ALIBI
+    };
+    MaskType maskType = MASK_TYPE_UNDEFINED;
+};
+
+
 void from_json(const nlohmann::json &paramJson, FlashAttentionRopeLayerParam &param);
 
 atb::Status FlashAttentionRopeLayer(const FlashAttentionRopeLayerParam &param, atb::Operation **operation);
