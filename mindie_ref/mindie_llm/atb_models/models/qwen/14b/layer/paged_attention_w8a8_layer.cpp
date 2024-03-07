425b12e75b642f18cfd0de80f517305c231b08df
superSY520521 <huangyuanxiang@huawei.com>
2024-03-07 17:05:41 +0000

    update mindie_ref/mindie_llm/atb_models/models/qwen/14b/layer/paged_attention_w8a8_layer.cpp.
    Signed-off-by: superSY520521 <huangyuanxiang@huawei.com>

--- mindie_ref/mindie_llm/atb_models/models/qwen/14b/layer/paged_attention_w8a8_layer.cpp
+++ mindie_ref/mindie_llm/atb_models/models/qwen/14b/layer/paged_attention_w8a8_layer.cpp
@@ -230,8 +230,11 @@ atb::Status PAW8A8Layer(const PAW8A8LayerParam &param, atb::Operation **operatio
     // c_proj(down)
     mlpParam.downLinearTensorParallelInfo = {param.rank, param.worldSize, param.backend};
     if (param.supportSwiGLU) {
+        mlpParam.activationParam.activationType = atb::infer::ActivationType::ACTIVATION_SWIGLU_FORWARD;
+        mlpParam.activationParam.dim = -1;
         MlpSwiGLU(mlpParam, &mlpParallelNode.operation);
     } else {
+        mlpParam.activationParam.activationType = atb::infer::ActivationType::ACTIVATION_SWISH;
         Mlp(mlpParam, &mlpParallelNode.operation);
     }
     mlpParallelNode.inTensorIds = {
