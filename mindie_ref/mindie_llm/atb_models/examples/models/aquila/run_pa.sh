def865454146ad7fd04422ec584e192a630a48ee
wangxinfei <wangxinfei@huawei.com>
2024-03-22 15:05:55 +0000

    !1137 910B Aquila跑精度问题修改 Merge pull request !1137 from wangxinfei/br_develop_mindie

--- mindie_ref/mindie_llm/atb_models/examples/models/aquila/run_pa.sh
+++ mindie_ref/mindie_llm/atb_models/examples/models/aquila/run_pa.sh
@@ -1,28 +1,28 @@
-#!/bin/bash
-# Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set -ex
 # Copyright Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 # 参数配置以及启动指令的说明见同级目录下的README.md文件
-export BIND_CPU=1
-export MAX_MEMORY_GB=30
 export ASCEND_RT_VISIBLE_DEVICES=4,5
 export MASTER_PORT=20030
-export TP_WORLD_SIZE=$(($(echo "${ASCEND_RT_VISIBLE_DEVICES}" | grep -o , | wc -l) +1))
-atb_options="ATB_LAUNCH_KERNEL_WITH_TILING=1 ATB_LAYER_INTERNAL_TENSOR_REUSE=1 PYTORCH_NPU_ALLOC_CONF='max_split_size_mb:2048' HCCL_BUFFSIZE=110"
-atb_async_options="ATB_OPERATION_EXECUTE_ASYNC=1 TASK_QUEUE_ENABLE=1"
-base_cmd="torchrun --nproc_per_node $TP_WORLD_SIZE --master_port $MASTER_PORT -m examples.run_pa --model_path $1 --max_prefill_tokens 64 --max_input_length 64"
-run_cmd="${atb_options} ${atb_async_options} ${base_cmd}"
-eval "${run_cmd}"
+export USE_REFACTOR=true
+
+# 以下环境变量与性能和内存优化相关，通常情况下无需修改
+export ATB_LAYER_INTERNAL_TENSOR_REUSE=1
+export INF_NAN_MODE_ENABLE=0
+export ATB_OPERATION_EXECUTE_ASYNC=1
+export TASK_QUEUE_ENABLE=1
+export ATB_CONVERT_NCHW_TO_ND=1
+export LCCL_ENABLE_FALLBACK=1
+export ATB_WORKSPACE_MEM_ALLOC_GLOBAL=1
+export ATB_CONTEXT_WORKSPACE_SIZE=0
+
+extra_param=""
+world_size=$(($(echo "${ASCEND_RT_VISIBLE_DEVICES}" | grep -o , | wc -l) +1))
+
+if [ "$USE_REFACTOR" = true ]; then
+    extra_param="${extra_param} --use_refactor True"
+fi
+
+if [ "$TP_WORLD_SIZE" == "1" ]; then
+    python -m examples.run_pa --model_path $1 $extra_param
+else
+    torchrun --nproc_per_node $world_size --master_port $MASTER_PORT -m examples.run_pa --model_path $1 $extra_param
+fi
