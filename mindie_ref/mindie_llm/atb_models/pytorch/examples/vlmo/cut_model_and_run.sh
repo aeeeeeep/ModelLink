a3efc73cbbad361f9ded8055c0c9c161701e5680
郭惜缘 <guoxiyuan1@huawei.com>
2024-03-26 16:38:25 +0800

    fix

--- mindie_ref/mindie_llm/atb_models/pytorch/examples/vlmo/cut_model_and_run.sh
+++ mindie_ref/mindie_llm/atb_models/pytorch/examples/vlmo/cut_model_and_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-input_dir="./vlmo/vlmo_base_patch16_480_vqa.pt"
-output_dir="./vlmo/part_model"
-
+input_dir="./vlmo"
+output_dir="${input_dir}/part_model"
 world_size=2
+
 max_seqence_length=4096
 use_launch_kernel_with_tiling=1
 atb_operation_execute_async=1
@@ -11,7 +11,7 @@ LONG_SEQ_ENABLE=0
 # ATB_LOG_TO_STDOUT=1 ATB_LOG_LEVEL=INFO TASK_QUEUE_ENABLE=0 ASDOPS_LOG_TO_STDOUT=1 ASDOPS_LOG_LEVEL=INFO
 atb_options="ATB_LAUNCH_KERNEL_WITH_TILING=1 ATB_LAYER_INTERNAL_TENSOR_REUSE=1 PYTORCH_NPU_ALLOC_CONF='max_split_size_mb:2048' HCCL_OP_BASE_FFTS_MODE_ENABLE=1 HCCL_BUFFSIZE=110"
 atb_async_options="ATB_OPERATION_EXECUTE_ASYNC=1 TASK_QUEUE_ENABLE=1"
-start_cmd="torchrun --nproc_per_node 2 --master_port 20001 cut_ascend_vqa.py with "task_finetune_vqa_base_image480"  test_only=True"
+start_cmd="torchrun --nproc_per_node 2 --master_port 20005 cut_run_vqa.py with "task_finetune_vqa_base_image480"  load_path="${input_dir}" test_only=True"
 run_cmd="${start_cmd}"
 # ${atb_options} ${atb_async_options}
 echo "**********************float model**********************"
