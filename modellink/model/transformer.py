062b9acdf64ac834430e877efdec098a8bb32564
shishaoyu <shishaoyu1@huawei.com>
2024-05-14 06:19:25 +0000

    update modellink/model/transformer.py.
    Signed-off-by: shishaoyu <shishaoyu1@huawei.com>

--- modellink/model/transformer.py
+++ modellink/model/transformer.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 # Copyright (c) 2024, HUAWEI CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
