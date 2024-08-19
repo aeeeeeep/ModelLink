6202d80382baee2df94a71d2ca9ab54f8dd35c4c
ryan <ruanzhixiang1@huawei.com>
2024-08-19 09:36:36 +0800

    feat: add llama spec to support llama series mcore model

---
+++ modellink/core/models/gpt/layer_specs.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2024, HUAWEI CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License..
+
+from megatron.core.fusions.fused_bias_dropout import get_bias_dropout_add
+from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParallelLinear
+from megatron.core.transformer.attention import SelfAttention, SelfAttentionSubmodules
+from megatron.core.transformer.dot_product_attention import DotProductAttention
+from megatron.core.transformer.enums import AttnMaskType
+from megatron.core.transformer.identity_op import IdentityOp
+from megatron.core.transformer.spec_utils import ModuleSpec
+from megatron.core.transformer.transformer_layer import TransformerLayer
+from megatron.core.models.gpt.gpt_layer_specs import _get_mlp_module_spec
+from megatron.training import get_args
+
+from modellink.core.transformer.custom_layers.transformer_engine import PTNorm
+from modellink.core.transformer.transformer_layer import TransformerLayerSubmodules
+
+args = get_args()
+num_experts, moe_grouped_gemm, qk_layernorm, use_flash_attn = (
+    args.num_experts, args.moe_grouped_gemm, args.qk_layernorm, args.use_flash_attn)
+
+# Transformer Layer Spec for gpt model
+gpt_layer_spec = ModuleSpec(
+    module=TransformerLayer,
+    submodules=TransformerLayerSubmodules(
+        input_layernorm=PTNorm,
+        self_attention=ModuleSpec(
+            module=SelfAttention,
+            params={"attn_mask_type": AttnMaskType.causal},
+            submodules=SelfAttentionSubmodules(
+                linear_qkv=ColumnParallelLinear,
+                core_attention=DotProductAttention,
+                linear_proj=RowParallelLinear,
+                q_layernorm=PTNorm if qk_layernorm else IdentityOp,
+                k_layernorm=PTNorm if qk_layernorm else IdentityOp,
+            ),
+        ),
+        self_attn_bda=get_bias_dropout_add,
+        pre_mlp_layernorm=PTNorm,
+        post_mlp_layernorm=PTNorm,
+        mlp=_get_mlp_module_spec(
+            use_te=False, num_experts=num_experts, moe_grouped_gemm=moe_grouped_gemm
+        ),
+        mlp_bda=get_bias_dropout_add,
+        sharded_state_dict_keys_map={
+            'input_layernorm.': 'self_attention.linear_qkv.layer_norm_',
+            'pre_mlp_layernorm.': 'mlp.linear_fc1.layer_norm_',
+        },
+    ),
+)
+
+# Transformer Layer in llama is identity to gpt layer spec
+llama_layer_local_spec = gpt_layer_spec
+
+
