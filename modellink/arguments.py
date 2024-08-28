08c715e72bc0262ec55a7065615a18f1b4648ca9
wenjiang2357 <wenjiang@huawei.com>
2024-08-28 16:51:30 +0800

    MiniCPM-2B/MiniCPM-MoE-8x2B支持

--- modellink/arguments.py
+++ modellink/arguments.py
@@ -59,7 +59,20 @@ def process_args(parser):
     parser = _add_mla_args(parser)
     parser = _add_yarn_args(parser)
     parser = _add_deepseek_moe_args(parser)
+    parser = _add_minicpm_args(parser)
 
+    return parser
+
+
+def _add_minicpm_args(parser):
+    group = parser.add_argument_group(title='minicpm_args')
+    group.add_argument('--scale-emb', type=float, default=None,
+                       help='scale embed tokens')
+
+    group.add_argument('--dim-model-base', type=float, default=None,
+                       help='dim-model-base')
+    group.add_argument('--scale-depth', type=float, default=None,
+                       help='scale-depth')
     return parser
 
 
