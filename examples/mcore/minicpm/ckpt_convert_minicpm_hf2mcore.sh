fb85b569833f2af734f315a70233e8d0f2098fc8
wenjiang2357 <wenjiang@huawei.com>
2024-08-29 16:28:34 +0800

    默认环境变量打开

--- examples/mcore/minicpm/ckpt_convert_minicpm_hf2mcore.sh
+++ examples/mcore/minicpm/ckpt_convert_minicpm_hf2mcore.sh
@@ -1,5 +1,5 @@
 # 修改 ascend-toolkit 路径
-#source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
 # 权重格式转换，设置需要的并行配置
 python tools/checkpoint/convert_ckpt.py \
