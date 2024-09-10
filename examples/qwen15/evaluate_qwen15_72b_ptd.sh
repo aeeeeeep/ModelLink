1a5bc851431c74db56d0939c0e57a64a8e0a9a44
changlei <changlei20@huawei.com>
2024-09-10 13:31:47 +0000

    update examples/qwen15/evaluate_qwen15_72b_ptd.sh.
    Signed-off-by: changlei <changlei20@huawei.com>

--- examples/qwen15/evaluate_qwen15_72b_ptd.sh
+++ examples/qwen15/evaluate_qwen15_72b_ptd.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# The number of parameters is not aligned
-export  LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib:/root/miniconda3/lib:$LD_LIBRARY_PATH
 export HCCL_CONNECT_TIMEOUT=1800
 export COMBINED_ENABLE=1
 export CUDA_DEVICE_MAX_CONNECTIONS=1
