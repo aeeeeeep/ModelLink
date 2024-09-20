336145af126dcfa5aa14ed5d5b7a7a7ec5d62cd4
RyanInnerpeace <ryan_innerpeace@foxmail.com>
2024-09-20 14:18:29 +0800

    refactor: use identity implement from mindspeed-core

--- modellink/patchs/megatron_patch.py
+++ modellink/patchs/megatron_patch.py
@@ -169,13 +169,13 @@ def patch_core_models(args):
 
 def patch_core_transformers(args):
     from mindspeed.core.transformer.moe.router import aux_loss_load_balancing
-    from ..core import (PTNorm, topk_router_forward, topk_router_routing, z_loss_func, rotary_embedding_init_wrapper)
     from mindspeed.core.transformer.moe.token_dispatcher import allgather_token_permutation, allgather_token_unpermutation
     from mindspeed.core.transformer.moe.grouped_gemm_util import Ops, grouped_gemm_is_available, get_device_capability
+    from mindspeed.core.transformer.transformer import core_mlp_forward_wrapper
 
+    from ..core import (PTNorm, topk_router_forward, topk_router_routing, z_loss_func, rotary_embedding_init_wrapper)
     from ..core.transformer.moe.moe_layer import moe_layer_init_wrapper, moe_layer_forward
     from ..core.transformer.transformer_block import _transformer_block_build_layers
-    from ..core.transformer.mlp import core_mlp_forward_wrapper
     from ..core.transformer.transformer_layer import transformer_layer_init_wrapper
 
     PatchManager.register_patch('torch.cuda.get_device_capability', get_device_capability)
